79fef3ac46955942178a8ceead98a225ba6e5060
usameturker <usameturker@gmail.com>
2022-11-07 13:19:13 -0500

    postgresql V2

--- create.sql
+++ create.sql
@@ -1,10 +1,26 @@
-/*create table students
-( id char(4),
- fname_lname varchar(30),
+create table students
+( id int primary key,
+ fname_lname varchar(30) not null,
  av_po real,
  reg_date date
-)*/
+)
 
-create table av_points
-as select fname_lname, av_po
-from students
+create table points(
+	student_id int check(student_id>0) not null,
+	points int not null,
+constraint avpo_fk foreign key(student_id) references students(id)
+on delete cascade)
+
+insert into points 
+values
+(1 , 89)
+(2 , 71)
+(3, 79 ),
+(4, 57 ),
+(5, 22 ),
+(6, 98 )
+
+update points
+set points = 65 where student_id=4
+
+select * from students where av_po in  (select points from points where points>60)
---
+++ select.sql
@@ -0,0 +1,6 @@
+select id, fname_lname || '= final point: ' || av_po || ' date is: ' || reg_date as infos from students 
+where id between 2 and 4
+select * from students
+
+
+
